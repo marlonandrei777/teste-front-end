6f402440c337e86cab0d9f2830fd408d16763e61
Marlon Andrei <marlon-master@hotmail.com>
2024-09-27 00:47:38 -0300

    :test_tube: test: Teste do component favorite-item concluido

--- src/app/components/favorite-item/favorite-item.component.spec.ts
+++ src/app/components/favorite-item/favorite-item.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FavoriteItemComponent } from './favorite-item.component';
+import { By } from '@angular/platform-browser';
 
 describe('FavoriteItemComponent', () => {
   let component: FavoriteItemComponent;
@@ -8,16 +9,36 @@ describe('FavoriteItemComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FavoriteItemComponent]
-    })
-    .compileComponents();
+      imports: [FavoriteItemComponent],
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(FavoriteItemComponent);
     component = fixture.componentInstance;
+
+    // simular o valor esperado
+    component.favorite = {
+      name: 'Belém',
+      temp: 22,
+      humidity: 60,
+      wind: 4,
+      description: 'ensolarado',
+    };
+
+    // aplicar as mudancas no componente
     fixture.detectChanges();
   });
 
+  // verifica se o componente foi criado com sucesso
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('tem que exibir os dados da cidade', () => {
+    const cityNameElement = fixture.debugElement.query(
+      By.css('.card')
+    ).nativeElement;
+    expect(cityNameElement.textContent).toContain('Belém'); // verificaa se o nome da cidade esta correto
   });
 });
